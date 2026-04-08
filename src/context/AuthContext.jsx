dd34ed363bd0e4b2836840f04714b8f4c852636d
Will Pinto <mitrabajo45@gmail.com>
2026-04-07 21:49:05 -0500

    feat: agregar kathearistizabald como administrador

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -13,8 +13,7 @@ const AuthContext = createContext();
 // Puedes agregar o quitar correos electrónicos de esta lista para gestionar los permisos de administrador.
 const ADMIN_EMAILS = [
     "pythonpcmocoa@gmail.com", // El correo administrador original
-    "kathearistizabald@gmail.com",
-    "pintowilmer138@gmail.com",  // Ejemplo de un segundo administrador
+    "kathearistizabald@gmail.com", //Este es el correo que quiero que tambien sea admin
     "supervisor@tutienda.com",  // Ejemplo de un tercer administrador
 ];
 
